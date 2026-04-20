1634477b2ff3738a276e96943e1740961c74a81f
ctrl8BySihab <sihab.workmail@gmail.com>
2026-04-20 14:58:59 +0600

    add /http_docs endpoint to return Scalar API reference

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
@@ -8,3 +9,8 @@ def get_detail():
         "content": "Power Supply Unit",
         "status": "Available"
     }
+
+
+@app.get("/http_docs", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(openapi_url= app.openapi_url, title="Scalar API Reference")
