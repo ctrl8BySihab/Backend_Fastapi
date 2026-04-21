d07c76e07557dc314410f102b7b01f8b884292b8
ctrl8BySihab <sihab.workmail@gmail.com>
2026-04-22 01:39:46 +0600

    Add route path parameter

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
+from typing import Any
 
 app = FastAPI()
 
-@app.get("/detail")
-def get_detail(): 
+@app.get("/shipment/{id}")
+def get_shipment(id: int) -> dict[str, Any]: 
     return {
+        "id": id,
+        "Weight": "2kg",
         "content": "Power Supply Unit",
         "status": "Available"
     }
