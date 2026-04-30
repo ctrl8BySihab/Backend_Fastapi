af10d6c0c1e18fa88d4e79d5deadb85a3d647f29
ctrl8BySihab <sihab.workmail@gmail.com>
2026-04-30 23:09:24 +0600

    Create shipmets table in the database

--- app/database.py
+++ app/database.py
@@ -1,5 +1,16 @@
 import sqlite3
 
 connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS shipments (
+        id INTEGER,
+        weight REAL,
+        content TEXT,
+        destination INTEGER,
+        status TEXT
+    )
+""")
 
 connection.close()
