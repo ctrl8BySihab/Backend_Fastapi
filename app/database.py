476740afabdaf9134fafd5074572e366cec330e9
ctrl8BySihab <sihab.workmail@gmail.com>
2026-04-30 22:55:56 +0600

    Create database.py and database.db

--- app/database.py
+++ app/database.py
@@ -1,17 +1,5 @@
-import json
+import sqlite3
 
-shipments = {}
+connection = sqlite3.connect("database.db")
 
-with open("shipments.json") as json_file:
-    orders = json.load(json_file)
-
-    for order in orders:
-        shipments[order["id"]] = order
-
-    print(shipments)
-
-
-def save():
-    with open("shipments.json", "w") as json_file:
-        json.dump(list(shipments.values()), json_file)
-
+connection.close()
