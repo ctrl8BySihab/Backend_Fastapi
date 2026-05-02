a6a3d0c943286f185b98f7eb6ae09e9d39a7364e
ctrl8BySihab <sihab.workmail@gmail.com>
2026-05-03 04:02:08 +0600

    Update query parameter

--- app/database.py
+++ app/database.py
@@ -7,25 +7,39 @@ connection = sqlite3.connect("database.db")
 # Create cursor to execute SQL commands
 cursor = connection.cursor()
 
-# Create the shipment table if it doesn't exist and insert some sample data
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS shipment (
-        id INTEGER,
-        weight REAL,
-        content TEXT,
-        destination INTEGER,
-        status TEXT
-    )
-""")
+# # Create the shipment table if it doesn't exist and insert some sample data
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS shipment (
+#         id INTEGER,
+#         weight REAL,
+#         content TEXT,
+#         destination INTEGER,
+#         status TEXT
+#     )
+# """)
 
-# Insert sample data into the shipment table
+# # Insert sample data into the shipment table
+# cursor.execute("""
+# INSERT INTO shipment
+# VALUES
+#     (12001, 10.5, 'Books', 101, 'placed'),
+#     (12002, 5.0, 'Clothing', 102, 'in_transit'),
+#     (12003, 2.0, 'Electronics', 103, 'out_for_delivery'),
+#     (12004, 15.0, 'Furniture', 104, 'delivered')
+# """)
+# # Commit the changes to the database
+# connection.commit()
+
+# # Display all shipments in the database
+# cursor.execute("""
+# SELECT * FROM shipment where id = 12002
+# """)
+# data = cursor.fetchall()
+# print(data)
+
+# Update the status of a shipment
 cursor.execute("""
-INSERT INTO shipment
-VALUES
-    (12001, 10.5, 'Books', 101, 'placed'),
-    (12002, 5.0, 'Clothing', 102, 'in_transit'),
-    (12003, 2.0, 'Electronics', 103, 'out_for_delivery'),
-    (12004, 15.0, 'Furniture', 104, 'delivered')
+UPDATE shipment SET status = "out_for_delivery" WHERE id = 12004
 """)
 # Commit the changes to the database
 connection.commit()
