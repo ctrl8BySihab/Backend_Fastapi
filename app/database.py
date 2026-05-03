cc60e4323ead911b1698f0b75f77833e070e3a36
ctrl8BySihab <sihab.workmail@gmail.com>
2026-05-04 03:13:19 +0600

    Add submit and update in database.py

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from typing import Any
+from app.schemas import ShipmentStatus, ShipmentRead, ShipmentCreate, ShipmentUpdate
 
 conn = sqlite3.connect("database.db")
 cur = conn.cursor()
@@ -8,7 +9,7 @@ cur = conn.cursor()
 class Database:
     def __init__(self):
         # Create connection and cursor
-        self.conn = sqlite3.connect("database.db")
+        self.conn = sqlite3.connect("database.db", check_same_thread=False)
         self.cur = self.conn.cursor()
 
         self.create_table()
@@ -30,7 +31,7 @@ class Database:
     def get(self, id: int) -> dict[str, Any] | None:
         self.cur.execute("""
             SELECT * FROM shipment
-            WHERE id =: id
+            WHERE id =:id
 
         """,
         {"id" : id}
@@ -45,10 +46,40 @@ class Database:
         } if data else None
     
     # Submitting new order
-    def submit(self, weight, content):
+    def submit(self, shipment: ShipmentCreate) -> int:
         self.cur.execute("""
-""")
-        pass
+            SELECT MAX(id) FROM shipment
+        """)
+        new_id = self.cur.fetchone()[0] + 1
+        
+        self.cur.execute("""
+            INSERT INTO shipment
+            VALUES (:id, :weight, :content, :status)
+        """,
+        {
+            "id": new_id,
+            **shipment.model_dump(),
+            "status": "placed"
+        }
+        )
+        self.conn.commit()
+        return new_id
+    
+    # Update order
+    def update(self, id: int, shipment: ShipmentUpdate) -> dict[str, Any] | None:
+        cur.execute("""
+            UPDATE shipment
+            SET status =:status
+            WHERE id =:id
+        """,
+        {"id" : id, **shipment.model_dump()}
+        )
+        self.conn.commit()
+        return self.get(id)
+    
+    
+    def
+
     # Close the connection
     def close(self):
         self.conn.close()
--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ def get_latest_shipment():
     id = max(shipments.keys())
     return {"id": id} | shipments[id]
 
-
 # Returns a single shipment by ID
 @app.get("/shipments", response_model= ShipmentRead)
 def get_shipment(id: int):
@@ -25,30 +24,22 @@ def get_shipment(id: int):
         )
     return shipment
 
-
 # Creates a new shipment with status "placed" and persists to JSON
 @app.post("/shipments", response_model=None)
 def submit_shipment(data: ShipmentCreate):
-    new_id = max(shipments.keys()) + 1
-    shipments[new_id] = {
-        "id": new_id,
-        **data.model_dump(),
-        "status": ShipmentStatus.placed
-    }
-    save()
+    new_id = db.submit(data)
     return {"details": f"Shipment with ID #{new_id} has been submitted", "id": new_id} | shipments[new_id]
-
 
 # Partially updates a shipment; only provided fields are changed
 @app.patch("/shipments", response_model=None)
 def update_shipment(id: int, data: ShipmentUpdate):
-    if id not in shipments:
+    shipment = db.get(id)
+    if shipment is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Shipment not found"
         )
-    shipments[id].update(**data.model_dump(exclude_none=True))
-    save()
-    return shipments[id]
+    shipment = db.update(id, data)
+    return shipment
 
 
 # Removes a shipment from the store and persists the change
