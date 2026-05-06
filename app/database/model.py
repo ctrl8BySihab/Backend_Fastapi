3b2bd4830c6b970f5f2472b289ec33a512d4917f
ctrl8BySihab <sihab.workmail@gmail.com>
2026-05-07 00:52:45 +0600

    Create shipment SQLmodel

--- app/database/model.py
+++ app/database/model.py
@@ -1,2 +1,10 @@
+from sqlmodel import Field, SQLModel
 
 
+class Shipment(SQLModel):
+    id: int
+    content: str
+    weight: float = Field(le=25)
+    destination: int
+    status: str
+    estimated_delivery: str
