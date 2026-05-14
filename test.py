b3939b32072b1ca2602c895e885baf05b11d8b0b
ctrl8BySihab <sihab.workmail@gmail.com>
2026-05-15 01:37:54 +0600

    Add test lifespan handler

---
+++ test.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from rich import print, panel
+
+@asynccontextmanager
+async def lifespan_handler(app: FastAPI):
+    print(panel.Panel("Server started...", border_style="green"))
+    yield
+
+    print(panel.Panel("...Server stopped", border_style="red"))
+
+
+app = FastAPI(lifespan=lifespan_handler)
+
+@app.get("/")
+def read_root():
+    return {
+        "details": "Server running!"
+    }
+
